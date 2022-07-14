e5ba113f4e925843460c44aa04618f384c09f74e
Leandro Almeida <leo.leandro.ars@gmail.com>
2022-07-14 19:19:16 -0300

    Implementação do Modal
    Co-authored-by: Leanddro13 <leanddrosouzasilva@gmail.com>

--- front/src/components/ContactForm.js
+++ front/src/components/ContactForm.js
@@ -1,25 +1,88 @@
-import { useState } from "react"
-import { criarContato } from "../services/contactService"
-import {BsFillXCircleFill} from "react-icons/bs"
-import "../style.css"
+import { useState } from "react";
+import { criarContato } from "../services/contactService";
+import {
+  BsFillXCircleFill,
+  BsFillPersonFill,
+  BsFillKeyFill,
+  BsTelephoneFill,
+  BsPerson
+} from "react-icons/bs";
+import { AiOutlineUser, AiOutlineMail } from "react-icons/ai";
+import "../style.css";
 
 export const ContactForm = (props) => {
-    const [contato, setContato] = useState({
-        nome: "",
-        telefone: "",
-        email: "",
-        foto: "",
-    })
+  const [contato, setContato] = useState({
+    nome: "",
+    telefone: "",
+    email: "",
+    foto: "",
+  });
 
-    const handleCreateContact = () => {
-        const token = sessionStorage.getItem("token")
-        criarContato(token, contato)
-    }
+  function hideModal() {
+    var element = document.getElementById("modal");
+    element.classList.remove("modalContainer");
+  }
 
-    return <div className="modalContainer" onClick={props.closeModal}>
-                <div className="modalContent"> 
-                    <span className="exitModal"><BsFillXCircleFill/> criar contato</span>
-                    
-                </div>
+  const handleCreateContact = () => {
+    const token = sessionStorage.getItem("token");
+    criarContato(token, contato);
+  };
+
+  return (
+    <div className="modalContainer" onClick={props.closeModal}>
+      <div className="modalContent">
+        <div className="add">
+          <span className="exitModal" onClick={hideModal}><BsFillXCircleFill/></span>
+          Criar contato
         </div>
-}
+        <div className="campos">
+            <div className="container-imagem-principal">
+                <img
+                className="container-imagem-modal"
+                src={contato.foto}
+                alt={<BsFillPersonFill />}
+                />
+            </div>
+          
+          <div className="input-Container">
+            <span>
+              <AiOutlineUser />
+            </span>
+            <input
+              className="inputAdicionar "
+              type="text"
+              placeholder="Nome"
+              name="nome"
+            ></input>
+          </div>
+
+          <div className="input-Container">
+            <span>
+              <AiOutlineMail />
+            </span>
+            <input
+              className="inputAdicionar "
+              type="text"
+              placeholder="E-mail"
+              name="email"
+            ></input>
+          </div>
+          
+          <div className="input-Container">
+            <span>
+              <BsTelephoneFill />
+            </span>
+            <input
+              className="inputAdicionar "
+              type="tel"
+              placeholder="Telefone"
+              name="T"
+            ></input>
+            <button className="botaoSalvar">Salvar</button>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
--- front/src/pages/Agenda.js
+++ front/src/pages/Agenda.js
@@ -1,114 +1,110 @@
-import React, { useEffect, useState } from "react"
-import { getContatos } from "../services/contactService"
+import React, { useEffect, useState } from "react";
+import { getContatos } from "../services/contactService";
 import {
-    BsBoxArrowRight,
-    BsPersonPlus,
-    BsFillPersonFill,
-    BsPencilSquare,
-    BsFillTrashFill,
-    BsSearch,
-} from "react-icons/bs"
+  BsBoxArrowRight,
+  BsPersonPlus,
+  BsFillPersonFill,
+  BsPencilSquare,
+  BsFillTrashFill,
+  BsSearch,
+} from "react-icons/bs";
 
-import "../style.css"
-import { ContactForm } from "../components/ContactForm"
+import "../style.css";
+import { ContactForm } from "../components/ContactForm";
 
 export function Agenda() {
-    const [contatos, setContatos] = useState([])
-    const [pesquisa, setPesquisa] = useState("")
-    const [isModalVisible, setIsModalVisible] = useState(false)
+  const [contatos, setContatos] = useState([]);
+  const [pesquisa, setPesquisa] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    useEffect(() => {
-        const reqContatos = async () => {
-            const listaContatos = await getContatos(
-                sessionStorage.getItem("token")
-            )
-            setContatos(listaContatos)
-        }
-        reqContatos()
-    }, [])
+  useEffect(() => {
+    const reqContatos = async () => {
+      const listaContatos = await getContatos(sessionStorage.getItem("token"));
+      setContatos(listaContatos);
+    };
+    reqContatos();
+  }, []);
 
-    const handlePesquisa = (e) => {
-        setPesquisa(e.currentTarget.value)
-    }
+  const handlePesquisa = (e) => {
+    setPesquisa(e.currentTarget.value);
+  };
 
-    const contatosFiltrados = pesquisa
-        ? contatos.filter((contato) => {
-              return contato.nome
-                  .toLowerCase()
-                  .startsWith(pesquisa.toLowerCase())
-          })
-        : contatos
+  const contatosFiltrados = pesquisa
+    ? contatos.filter((contato) => {
+        return contato.nome.toLowerCase().startsWith(pesquisa.toLowerCase());
+      })
+    : contatos;
 
-    return (
-        <div>
-            {isModalVisible ? (
-                <ContactForm closeModal={() => setIsModalVisible(false)} />
-            ) : null}
-            <menu>
-                <span className="titulo">Contatos</span>
-                <div className="logout">
-                    <button className="botaoSair">Desconectar</button>
-                    <span className="BsBoxArrowRight">
-                        <BsBoxArrowRight />
-                    </span>
-                </div>
-            </menu>
-            <div className="botaoAdd">
-                <button
-                    className="botaoAdicionar"
-                    onClick={() => setIsModalVisible(true)}>
-                    <span className="BsPersonPlus">
-                        <BsPersonPlus />
-                    </span>
-                    Adicionar
-                </button>
-            </div>
-            <main>
-                <div className="container-formulario">
-                    <span className="BsSearch">
-                        <BsSearch />
-                    </span>
-                    <input
-                        className="formulario"
-                        type="text"
-                        placeholder="Procure por alguém"
-                        value={pesquisa}
-                        onChange={handlePesquisa}></input>
-                </div>
-                <section>
-                    <div className="tabela-contatos-principal">
-                        <table className="tabela-contatos">
-                            <tbody>
-                                {contatosFiltrados.map((contato) => {
-                                    return (
-                                        <tr key={contato._id}>
-                                            <td className="apareca-700">
-                                                <div className="container-imagem-principal">
-                                                    <img
-                                                        className="container-imagem"
-                                                        src={contato.foto}
-                                                        alt={
-                                                            <BsFillPersonFill />
-                                                        }
-                                                    />
-                                                </div>
-                                            </td>
-                                            <td>{contato.nome}</td>
-                                            <td>{contato.telefone}</td>
-                                            <td>
-                                                <BsPencilSquare />
-                                            </td>
-                                            <td className="icone-vermelho">
-                                                <BsFillTrashFill />
-                                            </td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
-            </main>
+  return (
+    <div>
+      {isModalVisible ? (
+        <ContactForm closeModal={() => setIsModalVisible(false)} />
+      ) : null}
+      <menu>
+        <span className="titulo">Contatos</span>
+        <div className="logout">
+          <button className="botaoSair">Desconectar</button>
+          <span className="BsBoxArrowRight">
+            <BsBoxArrowRight />
+          </span>
         </div>
-    )
+      </menu>
+      <div className="botaoAdd">
+        <button
+          className="botaoAdicionar"
+          onClick={() => setIsModalVisible(true)}
+        >
+          <span className="BsPersonPlus">
+            <BsPersonPlus />
+          </span>
+          Adicionar
+        </button>
+      </div>
+      <div className="container-formulario">
+        <span className="BsSearch">
+          <BsSearch />
+        </span>
+        <input
+          className="formulario"
+          type="text"
+          placeholder="Procure por alguém"
+          value={pesquisa}
+          onChange={handlePesquisa}
+        ></input>
+      </div>
+      <main>
+        <section>
+          <div className="tabela-contatos-principal">
+            <table className="tabela-contatos">
+              <tbody>
+                {contatosFiltrados.map((contato) => {
+                  return (
+                    <tr key={contato._id}>
+                      <td className="apareca-700">
+                        <div className="container-imagem-principal">
+                          <img
+                            className="container-imagem-modal"
+                            src={contato.foto}
+                            alt={<BsFillPersonFill />}
+                          />
+                        </div>
+                      </td>
+                      <td>{contato.nome}</td>
+                      <td>{contato.telefone}</td>
+                      <td>
+                        <BsPencilSquare />
+                      </td>
+                      <td className="icone-vermelho">
+                        <BsFillTrashFill />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
